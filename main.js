a31bd16956c7046a0cf1a06588c056c6b374c3e6
Pierre Lingat <pierre@helloworld.college>
2021-08-30 01:18:26 +1000

    Added chat display and text, has a generic response now

--- main.js
+++ main.js
@@ -8,14 +8,23 @@ var chatInput = document.getElementById("chatInput");
 var chatDisplay = document.getElementById("chatDisplay");
 
 const ENTER = 13
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-   
-  if (event.keyCode === ENTER) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    chatDisplay.append("<p> " + chatInput.value + "</p>")
-    
-    
-  }
+console.log(chatDisplay)
+
+// Returns a string of the chatbots response
+function createResponse(userInput) {
+    return "I don't understand what '" + userInput + "' means."
+}
+// Execute a function when the user releases enter
+// Should trigger a response by our chatbot and append the users input 
+// to the display
+chatInput.addEventListener("keyup", function(event) {
+  
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        chatDisplay.innerHTML += "<p><b>You:</b>" + chatInput.value + "</p>"
+        // TODO: add delay
+        chatDisplay.innerHTML += "<p><b>Bot:</b>" + createResponse(chatInput.value) + "</p>"
+        chatInput.value = ""
+    }
 });
